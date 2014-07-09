1a335aa549b4fc7ea5247a9ba7ff4cc77342184e
Ilya Sergey <ilya.sergey@gmail.com>
2014-07-09 18:16:29 +0200

    some new files added

---
+++ coq/Induction.v
@@ -0,0 +1,6 @@
+(** %\chapter{Inductive Predicates and Proofs}% *)
+
+Require Import ssreflect ssrbool ssrnat eqtype ssrfun seq path.
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
---
+++ coq/Introduction.v
@@ -0,0 +1,9 @@
+(** %\chapter{Introduction}% *)
+
+(**
+
+The goal of this cours is to demonstrate how mathematics can benefit
+from the computer science and, in particular, from the concepts
+familiar from the mainstream programming langauges.
+
+ *)
