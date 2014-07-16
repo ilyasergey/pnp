ffcc9bd84c59885030186cedad1def7f006252ae
Ilya Sergey <ilya.sergey@gmail.com>
2014-07-16 18:54:06 +0200

    prop logic done

---
+++ coq/BoolReflect.v
@@ -0,0 +1,43 @@
+(** 
+
+%\chapter{The basics of boolean reflection}
+\label{ch:boolrefl}%
+
+*)
+
+
+(** * [Prop] versus [bool]
+
+TODO: Emphasize that in Prop you can use quantifiers, whereas [bool]
+is as expressive as simple propositional logic (which is its strength,
+thank to Coq's terminating computations)
+
+*)
+
+
+(** * The [reflect] data type
+
+Construct a simple reflection procedure for some simple user-specific
+connective.
+
+
+*)
+
+
+
+(** * Using conditionals in predicates
+
+TODO: Emphasize that in Prop you can use quantifiers, whereas [bool]
+is as expressive as simple propositional logic (which is its strength,
+thank to Coq's terminating computations)
+
+*)
+
+
+(** * Types with decidable equalities
+
+TODO: Emphasize that in Prop you can use quantifiers, whereas [bool]
+is as expressive as simple propositional logic (which is its strength,
+thank to Coq's terminating computations)
+
+*)
