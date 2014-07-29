e6297930d545d6cd5f62ac451e61baeab2b4a5b3
Ilya Sergey <ilya.sergey@gmail.com>
2014-07-29 19:12:02 +0200

    more stuff in

--- coq/HTT.v
+++ coq/HTT.v
@@ -3,7 +3,44 @@
 \label{ch:htt}
 % *)
 
+(* begin hide *)
+Module HTT.
+(* end hide *)
+
+(** remove printing ~ *)
+(** printing ~ %\textasciitilde% *)
+(** printing R $R$ *)
+(** printing done %\texttt{\emph{done}}% *)
+(** printing congr %\texttt{\emph{congr}}% *)
+(** printing of %\texttt{\emph{of}}% *)
+(** printing first %\texttt{{first}}% *)
+(** printing last %\texttt{\emph{last}}% *)
+(** printing suff %\texttt{\emph{suff}}% *)
+(** printing have %\texttt{\emph{have}}% *)
+(** printing View %\texttt{\emph{View}}% *)
+(** printing >-> %\texttt{>->}% *)
+
 Require Import ssreflect ssrbool ssrnat eqtype ssrfun seq path.
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
+
+(** 
+
+* Specifications of imperative programs and Hoare triples
+
+* Basics of Separation Logic
+
+* Encoding program specifications as types
+
+** The Hoare monad
+
+* Specifying and verifying single-linked lists
+
+* Further reading 
+
+*)
+
+(* begin hide *)
+End HTT.
+(* end hide *)
