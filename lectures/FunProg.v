6409816f3e5cffec5cc22047805b6c70217b9a62
Ilya Sergey <ilya.sergey@gmail.com>
2014-08-07 11:01:31 +0200

    more exercises for FunProg

--- lectures/FunProg.v
+++ lectures/FunProg.v
@@ -1,4 +1,4 @@
-(** %\chapter{Functional Programming in Coq}% *)
+(** %\chapter{Functional Programming in Coq} *)
 
 Module FunProg.
 
@@ -13,13 +13,41 @@ Inductive unit : Set := tt.
 
 Check tt.
 
+(**
+[[
+tt
+     : unit
+]]
+*)
+
 Check unit.
+
+(**
+[[
+unit
+     : Set
+]]
+*)
 
 Inductive empty : Set := .
 
 Require Import ssreflect ssrbool.
 
 Print bool.
+(** 
+[[
+Inductive bool : Set :=  true : bool | false : bool
+]] 
+*)
+
+(** 
+
+Let us now try to define some functions that operate with the bool
+datatype ignoring for a moment the fact that most of them, if not all,
+are already defined in the standard Coq/SSReflect library.  Our first
+function will simply negate the boolean value and return its opposite:
+
+*)
 
 Definition negate b := 
   match b with 
@@ -28,10 +56,20 @@ Definition negate b :=
   end.
 
 Check negate.
+(**
+[negate : bool -> bool
+]
 
-(** * Simple recursive datatypes and programs *)
+* Simple recursive datatypes and programs
+
+*)
 
 Print nat.
+
+(**
+[Inductive nat : Set :=  O : nat | S : nat -> nat]
+
+*)
 
 Require Import ssrnat.
 
@@ -42,16 +80,64 @@ Fixpoint my_plus n m :=
  end.
 
 Eval compute in my_plus 5 7. 
+(** 
+[  = 12 : nat] 
+*)
 
 Fixpoint my_plus' n m := if n is n'.+1 then (my_plus' n' m).+1 else m.
 
+(**
+[[
+Fixpoint my_plus_buggy n m := 
+    if n is n'.+1 then (my_plus_buggy n m).+1 else m.
+]]
+
+we immediately get the following error out of the Coq interpreter:
+
+[[
+Error: Cannot guess decreasing argument of fix.
+]]
+
+*)
+
+Check nat_rec.
+(** 
+[[
+nat_rec : forall P : nat -> Set,
+          P 0 -> (forall n : nat, P n -> P n.+1) -> forall n : nat, P n
+]]
+
+To see how [nat_rec] is implemented, let us explore its generalized
+version, [nat_rect]:
+
+*)
+
 Print nat_rect.
+(** 
+[[
+nat_rect = 
+ fun (P : nat -> Type) (f : P 0) (f0 : forall n : nat, P n -> P n.+1) =>
+ fix F (n : nat) : P n :=
+   match n as n0 return (P n0) with
+   | 0 => f
+   | n0.+1 => f0 n0 (F n0)
+   end
+      : forall P : nat -> Type,
+        P 0 -> (forall n : nat, P n -> P n.+1) -> forall n : nat, P n
+]]
+
+*)
 
 Definition my_plus1 n m := nat_rec (fun _ => nat) m (fun n' m' => m'.+1) n.
 
 Eval compute in my_plus1 16 12.
 
-(** ** Dependent function types and pattern matching *)
+(** 
+[    = 28 : (fun _ : nat => nat) 16]
+
+** Dependent function types and pattern matching
+
+*)
 
 Check nat_rec.
 
@@ -63,17 +149,97 @@ match n' return P n' -> _ with
    | n1.+1 => fun m => my_plus m (n'.+1) 
 end m) n.
 
+(*
+Definition three_to_unit n := 
+ let: P := (fun n => if n is 3 then unit else nat) in
+ nat_rec P 0 (fun n' _ => match n' return P n'.+1 with
+               | 2 => tt
+               | _ => n'.+1
+               end) n.
+
+Eval compute in three_to_unit 0.
+*)
+
 Eval compute in sum_no_zero 0.
 
+(** 
+
+[ 
+     = tt
+     : (fun n : nat => match n with
+                       | 0 => unit
+                       | _.+1 => nat
+                       end) 0
+]
+
+*)
+
 Eval compute in sum_no_zero 5.
+(** 
+[[
+     = 15
+     : (fun n : nat => match n with
+                       | 0 => unit
+                       | _.+1 => nat
+                       end) 5
+]]
+
+Had we omitted the [return] clauses in the pattern matching, we would
+get the following type-checking error, indicating that Coq cannot
+infer that the type of [my_plus]' argument is always [nat], so it
+complains:
+
+[[
+Definition sum_no_zero' n := 
+ let: P := (fun n => if n is 0 then unit else nat) in
+ nat_rec P tt (fun n' m => 
+match n' with
+   | 0 => fun _ => 1
+   | n1.+1 => fun m => my_plus m (n'.+1) 
+end m) n.
+]]
+
+[[
+Error:
+In environment
+n : ?37
+P := fun n : nat => match n with
+                    | 0 => unit
+                    | _.+1 => nat
+                    end : nat -> Set
+n' : nat
+m : P n'
+The term "m" has type "P n'" while it is expected to have type "nat".
+]]
+*)
 
 (** ** Recursion principle and non-inhabited types *)
 
 Check empty_rect.
 
+(** 
+[[
+empty_rect
+     : forall (P : empty -> Type) (e : empty), P e
+]]
+
+
+Assuming existence of a value, which \emph{cannot be constructed}, we
+will be able to construct \emph{anything}.
+ 
+*)
+
 Inductive strange : Set :=  cs : strange -> strange.
 
 Check strange_rect.
+
+(** 
+[[
+strange_rect
+     : forall P : strange -> Type,
+       (forall s : strange, P s -> P (cs s)) -> forall s : strange, P s
+]]
+*)
 
 (** * More datatypes *)
 
@@ -81,24 +247,87 @@ Check strange_rect.
 
 Check prod.
 
+(**
+[[
+prod : Type -> Type -> Type
+
+]]
+*)
+
 Print prod.
+
+(** 
+[[
+Inductive prod (A B : Type) : Type :=  pair : A -> B -> A * B
+
+For pair: Arguments A, B are implicit and maximally inserted
+For prod: Argument scopes are [type_scope type_scope]
+For pair: Argument scopes are [type_scope type_scope _ _]
+]]
+*)
 
 Check pair 1 tt.
 
+(** 
+[[
+(1, tt) : nat * unit
+
+]]
+
+If one wants to explicitly specify the type arguments of a
+constructor, the [@]-prefixed notation can be used:
+
+*)
+
 Check @pair nat unit 1 tt.
 
+(**
+[[
+
+(1, tt) : nat * unit
+
+]]
+
+*)
+
 Check fst.
+(**
+[[
+fst : forall A B : Type, A * B -> A
+]]
+*)
 
 Check snd.
+(**
+[[
+fst : forall A B : Type, A * B -> A
+]]
 
-(* Products  *)
+The notation "[_ * _]" is not hard-coded into Coq, but rather is
+defined as a lightweight syntactic sugar on top of standard Coq
+syntax. Very soon we will see how one can easily extend Coq's syntax
+by defining their own notations. We will also see how is it possible
+to find what a particular notation means.
+*)
 
 Print sum.
-
-(* Lists *)
+(**
+[[
+Inductive sum (A B : Type) : Type :=  inl : A -> A + B | inr : B -> A + B
+]]
+*)
 
 Require Import seq.
 Print seq.
+
+(** 
+[[
+Notation seq := list
+]]
+
+%\ssrd{list}%
+
+*)
 
 Print list.
 
@@ -106,37 +335,6 @@ Print list.
 [[
 Inductive list (A : Type) : Type := nil : list A | cons : A -> list A -> list A
 ]]
-*)
-
-(**
-%\begin{exercise}[Fun with lists in Coq]%
-
-Implement the recursive function [alternate] of type [seq nat -> seq
-nat -> seq nat], so it would construct the alternation of two
-sequences according to the following "test cases".
-
-Eval compute in alternate [:: 1;2;3] [:: 4;5;6].
-[[
-     = [:: 1; 4; 2; 5; 3; 6]
-     : seq nat
-]]
-
-Eval compute in alternate [:: 1] [:: 4;5;6].
-[[
-     = [:: 1; 4; 5; 6]
-     : seq nat
-]]
-Eval compute in alternate [:: 1;2;3] [:: 4].
-[[
-     = [:: 1; 4; 2; 3]
-     : seq nat
-]]
-
-%\hint% The reason why the "obvious" elegant solution might fail is
- that the argument is not strictly decreasing.
-
-%\end{exercise}%
-
 *)
 
 (** * Searching for definitions and notations *)
@@ -300,8 +498,23 @@ fact
 ]]
 *)
 
-(** 
+(** * Exercises on functional programming in Coq *)
 
+(**
+
+---------------------------------------------------------------------
+%\begin{exercise}%
+
+Write the function [two_power] of type [nat -> nat], such that
+[two_power n = 2^n]. Use the functions that we have defined earlier.
+
+%\end{exercise}%
+---------------------------------------------------------------------
+
+*)
+
+
+(**
 TODO: more exercises
 
 - More standard list functions
@@ -319,7 +532,39 @@ Write a function that akes a number [n] as an argument... TODO
 %\end{exercise}%
 ---------------------------------------------------------------------
 
+(**
+---------------------------------------------------------------------
+%\begin{exercise}[Fun with lists in Coq]%
 
+Implement the recursive function [alternate] of type [seq nat -> seq
+nat -> seq nat], so it would construct the alternation of two
+sequences according to the following "test cases".
+
+Eval compute in alternate [:: 1;2;3] [:: 4;5;6].
+[[
+     = [:: 1; 4; 2; 5; 3; 6]
+     : seq nat
+]]
+
+Eval compute in alternate [:: 1] [:: 4;5;6].
+[[
+     = [:: 1; 4; 5; 6]
+     : seq nat
+]]
+Eval compute in alternate [:: 1;2;3] [:: 4].
+[[
+     = [:: 1; 4; 2; 3]
+     : seq nat
+]]
+
+%\hint% The reason why the "obvious" elegant solution might fail is
+ that the argument is not strictly decreasing.
+
+%\end{exercise}%
+---------------------------------------------------------------------
+*)
+
+(**
 ---------------------------------------------------------------------
 %\begin{exercise}%[Functions with dependently-typed result type]
 
--- solutions/FunProg_solutions.v
+++ solutions/FunProg_solutions.v
@@ -1,5 +1,58 @@
 Require Import ssreflect ssrbool ssrnat.
 
+
+(**
+---------------------------------------------------------------------
+%\begin{exercise}%
+
+Write the function [two_power] of type [nat -> nat], such that
+[two_power n = 2^n]. Use the functions that we have defined earlier.
+
+%\end{exercise}%
+---------------------------------------------------------------------
+
+*)
+
+Fixpoint two_power n := match n with 
+  | 0 => 1
+  | m.+1 => my_mult 2 (two_power m)
+  end.
+
+(** 
+---------------------------------------------------------------------
+%\begin{exercise}[Fun with lists in Coq]%
+
+Implement the recursive function [alternate] of type [seq nat -> seq
+nat -> seq nat], so it would construct the alternation of two
+sequences.
+
+%\hint% The reason why the "obvious" elegant solution might fail is
+ that the argument is not strictly decreasing.
+
+%\end{exercise}%
+---------------------------------------------------------------------
+*)
+
+Fixpoint alternate (l1 l2 : seq nat) : seq nat := 
+match (l1, l2) with 
+  | ([::], [::]) => [::]
+  | ([::], ys) => ys
+  | (xs, [:: ]) => xs
+  | (x::xs, y::ys) => x :: y :: (alternate xs ys)
+  end. 
+
+(**
+---------------------------------------------------------------------
+%\begin{exercise}%[Functions with dependently-typed result type]
+
+Write a function that has a dependent result type and whose result is
+[true] for natural numbers of the form [4n + 1], [false] for numbers
+of the form [4n + 3] and [n] for numbers of the from [2n].
+
+%\end{exercise}%
+---------------------------------------------------------------------
+*)
+
 Fixpoint dep_type (n : nat) : Set := match n with 
   | 0 => nat
   | 1 => bool
